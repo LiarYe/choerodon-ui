298021fe9aeeb8ae7c243610878dd54bceaf649b
yezongjie <540852754@qq.com>
2026-09-03 15:59:04 +0800

    [IMP] configure: Modify the default value of duplicateKey.

--- components/configure/default.tsx
+++ components/configure/default.tsx
@@ -100,7 +100,7 @@ const defaults = {
   collapseExpandIconPosition: 'left',
   collapseTrigger: 'header',
   lovTableProps: {},
-  duplicateKey: { disable: true, showWarning: true },
+  duplicateKey: { disable: false, showWarning: true },
   lovModalProps: {},
   lovModalAutoWidth: false,
   lovAutoSelectSingle: false,
